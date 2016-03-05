9c96d26d05d79325a50acc0426a859226116db42
Joe <joewillsher@icloud.com>
2016-03-05 16:06:06 +0000

    Buffer resizing for stack

--- Vist/Compiler/Test.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
+++ Vist/Compiler/Test.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -5,74 +5,86 @@ extension UnsafeMutablePointer {
     mutating func recedeByAlignOf<T>(el: T.Type) { self = advancedBy(-alignof(el)) }
 }
 
-struct Stack {
+final class Stack {
     var stack, stackPtr, basePtr: UnsafeMutablePointer<Int8>
     /// The maximum size of stack allocated
-    var capacity: Int = 10
+    var capacity: Int
+    var bitsOccupied: Int { return offset/8 }
+    
     /// The offset of the stack pointer
     var offset: Int { return stack.distanceTo(stackPtr) }
+    /// The offset of the base pointer
+    var frameOffset: Int { return stack.distanceTo(basePtr) }
     
     init() {
+        capacity = 1
         stack = UnsafeMutablePointer<Int8>.alloc(capacity)
         stackPtr = stack
         basePtr = stackPtr
     }
     
+    /// Moves the stack into a new buffer twice the old size
+    private func initialiseNewBuffer() {
+        let oldCapacity = capacity
+        capacity *= 2
+        let newStack = UnsafeMutablePointer<Int8>.alloc(capacity)
+        newStack.moveInitializeFrom(stack, count: oldCapacity)
+        stackPtr = stack.advancedBy(offset)
+        basePtr = stack.advancedBy(frameOffset)
+        stack = newStack
+    }
+    
+    deinit {
+        stack.dealloc(capacity)
+    }
+    
     /// Pushes an element onto the stack
-    mutating func push<T>(element: T) {
-        stackPtr.advanceByAlignOf(T)
+    func push<T>(element: T) {
+        if bitsOccupied >= capacity { initialiseNewBuffer() }
         let ptr = unsafeBitCast(stackPtr, UnsafeMutablePointer<T>.self)
         ptr.initialize(element)
+        stackPtr.advanceByAlignOf(T)
     }
     
     /// Pops an element of type `type` and moves the stack
     /// pointer backwards
-    mutating func pop<T>(type: T.Type) -> T {
-        let ptr = unsafeBitCast(stackPtr, UnsafeMutablePointer<T>.self)
+    func pop<T>(type: T.Type) -> T {
+        let val = read(T)
         stackPtr.recedeByAlignOf(T)
-        return ptr.memory
+        return val
     }
     
     /// Reads the current stack pointer as `type`
     func read<T>(type: T.Type) -> T {
-        return unsafeBitCast(stackPtr, UnsafeMutablePointer<T>.self).memory
+        return unsafeBitCast(stackPtr, UnsafeMutablePointer<T>.self).predecessor().memory
     }
 
     /// Adds a new stack frame
     func call() {
         
     }
-    
+
+    var description: String {
+        let stackDesc = stack.stride(to: stackPtr, by: 1)
+            .map({ String($0.memory) })
+            .joinWithSeparator("")
+        let o = Array(count: offset-1, repeatedValue: " ")
+            .joinWithSeparator("")
+        return "\(stackDesc)\n\(o)^\(stackPtr)"
+    }
 }
 
 // tests
 
-var s = Stack()
+let s = Stack()
 
-s.stackPtr // 0x7FBA43520A10
+for a in 0..<10 {
+    s.push(a)
+}
 
-s.push(1)
-s.push(Int32(2))
-s.push(false)
-
-// stack: 00000001 , 0001 , 0
-
-s.stackPtr // 0x7FBA43520A1D
-s.read(Bool) // false
-s.pop(Bool) // false
-
-// stack: 00000001 , 0002
-
-s.stackPtr // 0x7FBA43520A1C
-s.read(Int32) // 2
-s.pop(Int32) // 2
-
-// stack: 00000001
-
-s.stackPtr // 0x7FBA43520A18
-s.read(Int) // 1
-s.pop(Int) // 1
-
+for a in 0..<10 {
+    s.pop(Int)
+}
 
 
 
