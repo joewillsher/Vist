c86005dda852bcc09bb8297b5c350d3643bbe763
Joe <joewillsher@icloud.com>
2016-05-30 17:49:17 +0100

    Remove metadata notes

--- Vist/stdlib/runtime/Metadata.cpp
+++ Vist/stdlib/runtime/Metadata.cpp
@@ -31,52 +31,6 @@ vist_getWitnessMethod(ExistentialObject *existential,
     return *(void**)f;
 }
 
-/*
- @_gFooconfCpropWitnessOffsetArr0 = constant i32 0
- @_gFooconfCpropWitnessOffsetArr = constant [2 x i32*] 
-    [
-        i32* @_gFooconfCpropWitnessOffsetArr0, i32* @_gFooconfCpropWitnessOffsetArr0
-    ]
- 
- @_gFooconfCwitnessTablewitnessArr0 = constant { i8* } 
-    { 
-        i8* bitcast (
-            void (%Foo*, %Int)* @foo_mFooI to i8*
-        )
-    }
- @_gFooconfCwitnessTablewitnessArr = constant [1 x { i8* }*] 
-    [
-        { i8* }* @_gFooconfCwitnessTablewitnessArr0
-    ]
- @_gFooconfCwitnessTable = constant { { i8* }*, i32 } 
-    { 
-        { i8* }* bitcast (
-            [1 x { i8* }*]* @_gFooconfCwitnessTablewitnessArr to { i8* }*
-        ),
-        i32 1 
-    }
- @_gFooconfC = constant { i8*, i32*, i32, { { i8* }*, i32 }* } 
-    { 
-        i8* bitcast (
-            { { i8*, i32*, i32, { { i8* }*, i32 }* }**, i32, i8* }* @_gFooconfCconcept to i8*
-        ), 
-        i32* bitcast (
-            [2 x i32*]* @_gFooconfCpropWitnessOffsetArr to i32*
-        ), 
-        i32 2, 
-        { { i8* }*, i32 }* @_gFooconfCwitnessTable 
-    }
- */
-
-
-//extern "C"
-//int32_t
-//vist_getPropertyOffset(ExistentialObject *existential,
-//                      ) {
-//    return offset...;
-//}
-
-
 #ifdef TESTING
 
 int main() {
